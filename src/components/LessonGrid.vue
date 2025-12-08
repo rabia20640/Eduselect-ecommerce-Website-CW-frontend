a87ef4268d5e5f880fae441dfd787d98a343bc84
Rabia Cheema <rabiacheema085@gmail.com>
2025-12-08 15:49:09 +0000

    Lesson structure update

--- src/components/LessonGrid.vue
+++ src/components/LessonGrid.vue
@@ -1,12 +1,15 @@
 <template>
   <!--Section container for showing a grid of lesson cards -->
   <section class="lesson-grid">
+    <!-- Loading and error states -->
+     <p v-if="loading"> Loading lessons... </p>
+     <p v-if="error">{{ error }}</p>
     <!-- Loop through each lesson in the lessons prop and render a LessonCard component -->
     <lesson-card
       v-for="lesson in lessons"
-      :key="lesson.id"
+      :key="lesson._id"
       :lesson="lesson"
-      @add-to-cart="$emit('add to cart', $event)"
+      @add-to-cart="$emit('add-to-cart', $event)"
     />
   </section>
 </template>
@@ -15,10 +18,34 @@
 <script>
 // Import the LessonCard component used to show individual lesson details
 import LessonCard from "./LessonCard.vue";
+ import { fetchLessons } from "../api/api.js"
 export default {
-  // Declare props recieved from parent component
-  props: ["lessons"],
-  // Register LessonCard as a local component
+  name: "LessonGrid",
   components: { LessonCard },
+  data() {
+    return {
+      lessons: [],
+      loading: true,
+      error: null,
+    };
+  },
+  async created() {
+    try {
+      this.lessons = await fetchLessons(); // call backend
+    } catch (err) {
+      this.error = "Failed to load lessons.";
+      console.error(err);
+    } finally {
+      this.loading = false;
+    }
+  },
 };
 </script>
+
+<style scoped>
+.error {
+  color: red;
+  font-weight: bold;
+}
+</style>
+
