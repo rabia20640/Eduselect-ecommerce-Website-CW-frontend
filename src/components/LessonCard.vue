c9779869d0017c03291bdc0d8ef74432ba103dba
Rabia Cheema <rabiacheema085@gmail.com>
2025-12-08 15:52:21 +0000

    Lesson Card Update

--- src/components/LessonCard.vue
+++ src/components/LessonCard.vue
@@ -1,8 +1,14 @@
 <template>
   <!-- Lesson card container styled with css -->
   <div class="lesson-card">
+    <div class="card-content">
 
-    <!-- Shows the subject of the lesson -->
+    <!-- Shows the icons for each lesson card -->
+     <img :src="lesson.image" :alt="lesson.subject" class="lesson-icon" /> 
+
+    <!-- Text on the right -->
+     <div class="lesson-details">
+     <!-- Shows the subject of the lesson -->
     <h3>{{ lesson.subject }}</h3>
 
     <!-- Shows the location of the lesson -->
@@ -12,11 +18,18 @@
     <p>Price: £{{ lesson.price }}</p>
     
     <!-- Shows how many spaces are available-->
-    <p>spaces: {{ lesson.spaces }}</p>
-    <p v-if="lesson.spces ===0" class="full-message">Fully booked</p>
+    <p>Spaces: {{ lesson.spaces }}</p>
+    <p v-if="lesson.spaces ===0" class="full-message">Fully booked</p>
 
     <!-- Emits an event to add this lesson to the cart -->
-    <button: disabled="lesson.spaces === 0" @click="$emit('add-to-cart', lesson)">Add to cart</button:>
+    <button 
+    @click="$emit('add-to-cart', lesson)"
+    :disabled="lesson.spaces === 0"
+    >
+    Add to cart
+  </button>
+  </div>
+  </div>
   </div>
 </template>
 
@@ -26,3 +39,40 @@ export default {
   props: ["lesson"],
 };
 </script>
+
+<style scoped>
+button {
+  background-color: #0078d4;
+  color: white;
+  border: none;
+  padding: 8px 14px;
+  border-radius: 6px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #005fa3;
+}
+
+button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+.lesson-icon {
+  width: 50px;
+  height: 50px;
+  object-fit: contain;
+}
+
+.card-content {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+
+.lesson-details {
+  flex-grow: 1;
+}
+</style>
